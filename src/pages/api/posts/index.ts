58972036a3619caf8af60ba07ab0c0f3b5d33b49
hamlet maharjan <hamletmaharjan@lftechnology.com>
2024-03-28 21:24:49 +0545

    Remove typeorm and setup prisma

---
+++ src/pages/api/posts/index.ts
@@ -0,0 +1,11 @@
+import type { NextApiRequest, NextApiResponse } from "next";
+import { PrismaClient } from "@prisma/client";
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  const prisma = new PrismaClient();
+  const posts = await prisma.post.findMany();
+  res.status(200).json(posts);
+}
