adeed5f4613c98001d6b08d7e8f3513eeb26f78b
hamlet maharjan <hamletmaharjan@lftechnology.com>
2024-03-29 17:07:58 +0545

    Highlight active link

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,16 +1,46 @@
 "use client";
+
 import { Flex, Box, Text, Link } from "@chakra-ui/react";
+import { usePathname } from "next/navigation";
 import NextLink from "next/link";
 
+const styles = {
+  activeLink: {
+    color: "green",
+    p: "12px",
+  },
+  link: {
+    p: "12px",
+  },
+};
+
 export default function Navbar() {
+  const pathname = usePathname();
+
   return (
     <Flex justifyContent={"space-between"} py="20px">
       <Flex>
-        <Text p="12px">Home</Text>
-        <Text p="12px">About</Text>
+        <Link
+          sx={pathname === "/" ? styles.activeLink : styles.link}
+          as={NextLink}
+          href="/"
+        >
+          Home
+        </Link>
+        <Link
+          sx={pathname === "/about" ? styles.activeLink : styles.link}
+          as={NextLink}
+          href="/about"
+        >
+          About
+        </Link>
       </Flex>
       <Flex>
-        <Link as={NextLink} href={"/create"} p="12px">
+        <Link
+          as={NextLink}
+          href={"/create"}
+          sx={pathname === "/create" ? styles.activeLink : styles.link}
+        >
           Create
         </Link>
         <Text p="12px">Signin/Signout</Text>
